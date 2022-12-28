467dd8c1b2d559c047c38b70c267c48ba584f5fa
khishh <khiraishi0319@gmail.com>
2022-12-28 11:09:48 -0800

    shared .env and .env.example file across root/web/backend using symlink

--- backend/server.go
+++ backend/server.go
@@ -20,12 +20,12 @@ const defaultPort = "8080"
 func main() {
 	godotenv.Load()
 	config := &database.Config{
-		Host:     os.Getenv("DB_HOST"),
-		Port:     os.Getenv("DB_PORT"),
-		Password: os.Getenv("DB_PASS"),
-		User:     os.Getenv("DB_USER"),
-		SSLMode:  os.Getenv("DB_SSLMODE"),
-		DBName:   os.Getenv("DB_NAME"),
+		Host:     os.Getenv("POSTGRES_HOST"),
+		Port:     os.Getenv("POSTGRES_PORT"),
+		Password: os.Getenv("POSTGRES_PASS"),
+		User:     os.Getenv("POSTGRES_USER"),
+		SSLMode:  os.Getenv("POSTGRES_SSLMODE"),
+		DBName:   os.Getenv("POSTGRES_DB_NAME"),
 	}
 	log.Printf("%s %s\n", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
 	db, err := database.NewConnection(config)
@@ -52,13 +52,6 @@ func main() {
 		BookRepository: repository.NewBookService(db),
 		UserRepository: repository.NewUserService(db),
 	}}))
-
-	// mux := http.NewServeMux()
-	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	w.Header().Set("Content-Type", "application/json")
-	// })
-
-	// handler := cors.Default().Handler(mux)
 
 	router.Handle("/graphiql", playground.Handler("GraphQL playground", "/api"))
 	router.Handle("/api", srv)
